c88d47e07a1c4b16ffbffcaa639f1be61758e9f9
sarangcsy <sarangcsy@vegemil.co.kr>
2025-03-07 17:26:02 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
+from tkinter.font import names
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
